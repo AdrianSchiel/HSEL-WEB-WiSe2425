126ed3392e66e1619f5dcf1d0fdf11b984b87e48
Adrian Schiel <adrian.schiel.2904@gmail.com>
2024-12-04 11:41:51 +0100

    Game Component

---
+++ components/gc-game.js
@@ -0,0 +1,75 @@
+class GCGame extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.updateContent();
+  }
+
+  static get observedAttributes() {
+    return ["image", "description", "title"];  
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.updateContent();
+  }
+
+  updateContent() {
+    const imgsrc = this.getAttribute("image");
+    const title = this.getAttribute("title");
+    const description = this.getAttribute("description");
+
+    const styles = `
+          img{
+           width: 100%;
+    height: auto; /* Beibehaltung des Seitenverhältnisses */
+    border-radius: 15px;
+    object-fit: cover; /* Alternative: 'contain', falls Ränder erlaubt sind */
+    display: block;
+          }
+          h1 {
+                text-transform: uppercase;
+                margin-bottom: 20px;
+                font-weight: 600;
+                font-size: 2rem;
+                font-family: 'Roboto',Arial,sans-serif;
+          }
+
+          p {
+                padding-top: 10px;
+                font-size: 1.2rem;
+                line-height: 1.6;
+          }
+          @media only screen and (max-width: 1160px) {
+            img {
+              width: 100%;
+            }
+
+            h1 {
+              font-size: 1.5rem;
+            }
+
+            p {
+              font-size: 1rem;
+            }
+          }
+    `;
+
+    const template = `
+          <img src="${imgsrc}" alt="" />
+          <div class="game-info">
+              <h1>${title}</h1>
+              <p>${description}</p>
+          </div>
+    `;
+
+    this.shadowRoot.innerHTML = `
+      <style>${styles}</style>
+      ${template}
+    `;
+  }
+}
+customElements.define("gc-game", GCGame);
